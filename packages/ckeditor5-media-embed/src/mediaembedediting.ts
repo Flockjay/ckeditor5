c987975047872600d898b403cbee5bed317e14b9
plsdev89 <plsdev89@gmail.com>
2024-02-20 19:10:18 -0500

    Add min width

--- packages/ckeditor5-media-embed/src/mediaembedediting.ts
+++ packages/ckeditor5-media-embed/src/mediaembedediting.ts
@@ -322,7 +322,7 @@ export default class MediaEmbedEditing extends Plugin {
 						const url = `${ domain }/embed/?contentId=${ contentId }&contentType=${ contentType }`;
 
 						return (
-							`<a href=${ match[ 0 ] } style="display: block; width: 100%; height: 100%; max-width: 298px; margin: auto;">` +
+							`<a href=${ match[ 0 ] } style="display: block; width: 100%; height: 100%; max-width: 298px; min-width: 210px; margin: auto;">` +
 								'<div style="position: relative; padding-bottom: calc(56.25% + 78px); pointer-events: none; border-radius: 8px; overflow: hidden;">' +
 									`<iframe src="${ url }"` +
 										'style="position: absolute; width: 100%; height: 100%; top: 0; left: 0;" ' +
